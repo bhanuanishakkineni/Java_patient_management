c3834786e44ea9cbeff94ef40edc4caea082e0fd
Bhanu Anish <bhanuanishakkineni77@gmail.com>
2026-03-31 19:26:43 -0500

    Add patient service

---
+++ patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
@@ -0,0 +1,24 @@
+package com.pms.patientservice.service;
+
+import com.pms.patientservice.dto.PatientResponseDTO;
+import com.pms.patientservice.models.Patient;
+import com.pms.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+import com.pms.patientservice.mapper.PatientMapper;
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+
+    
+}
